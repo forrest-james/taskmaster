d456f968f7895c18f57f95a7677ad74071cf6ee2
James Forrest <61599518+forrest-james@users.noreply.github.com>
2021-01-12 21:22:19 -0600

    Built Application Interfaces and Result model
    Interfaces
    - IApplicationDbContext
    - ICurrentUserService
    - IIdentityService

---
+++ source/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<JobApplication> JobApplications { get; set; }
+        DbSet<ToDoList> ToDoLists { get; set; }
+        DbSet<ToDoItem> ToDoItems { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
